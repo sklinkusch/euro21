d90cef03312ecedd1dadb5a3d01556feb67149ae
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-06-22 19:37:33 +0200

    add fontWeights

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,6 +1,10 @@
 const theme = {
   breakpoints: ["420px", "1000px", "1440px"],
-  fontSizes: [14, 16, 24],
+  fontSizes: [14, 16, 24, 32],
+  fontWeights: {
+    "regular": 400,
+    "bold": 700
+  },
   colors: {
     "bg": "antiquewhite",
     "ft": "black",
