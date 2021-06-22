71202312f21b89a2792da99350db1fdb9e15c362
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-06-22 19:22:53 +0200

    initial theme

---
+++ src/theme/theme.js
@@ -0,0 +1,10 @@
+const theme = {
+  breakpoints: ["420px", "1000px", "1440px"],
+  fontSizes: [14, 16, 24],
+  colors: {
+    "bg": "antiquewhite",
+    "ft": "black",
+  },
+}
+
+export default theme
