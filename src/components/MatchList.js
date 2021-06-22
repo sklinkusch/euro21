1204944ffa753505b0a1613f427d0fe6dd956e5c
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-06-22 19:56:52 +0200

    restyle MatchList

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,39 +1,40 @@
+/** @jsxImportSource theme-ui */
 import React from 'react'
 import Flag from "react-world-flags"
-import {participantName as participants} from "./countries"
+import { participantName as participants } from "./countries"
+import { getColor } from "./helpers"
 
-function MatchList({matches}){
-  const { A: groupA, B: groupB, C: groupC, D: groupD, E: groupE, F: groupF } = matches 
+function MatchList({ matches }) {
+  const { A: groupA, B: groupB, C: groupC, D: groupD, E: groupE, F: groupF } = matches
   return (
-    <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr", gridTemplateRows: "repeat(3,1fr)" }}>
-      <GroupMatchList matches={groupA} border={true} number={0} />
-      <GroupMatchList matches={groupB} border={true} number={1} />
-      <GroupMatchList matches={groupC} border={true} number={2} />
-      <GroupMatchList matches={groupD} border={true} number={3} />
-      <GroupMatchList matches={groupE} border={false} number={4} />
-      <GroupMatchList matches={groupF} border={false} number={5} />
+    <div sx={{ display: "grid", gridTemplateColumns: ["repeat(2,1fr)", "repeat(3,1fr)", "repeat(2,1fr)"], gridTemplateRows: ["repeat(3,1fr)", "repeat(2,1fr)", "repeat(3,1fr)"] }}>
+      <GroupMatchList matches={groupA} number={0} />
+      <GroupMatchList matches={groupB} number={1} />
+      <GroupMatchList matches={groupC} number={2} />
+      <GroupMatchList matches={groupD} number={3} />
+      <GroupMatchList matches={groupE} number={4} />
+      <GroupMatchList matches={groupF} number={5} />
     </div>
   )
 }
 
-function GroupMatchList({matches, border, number}) {
-  const borderLeft = number % 2 === 1 ? "1px solid black" : "none"
-  const borderBottom = border ? "1px solid black" : "none"
+function GroupMatchList({ matches, border, number }) {
+  const bgcolor = getColor(number)
   return (
-    <table style={{ width: "100%", borderBottom, borderLeft }}>
+    <table sx={{ width: "100%", my: "4px", mx: "4px", py: "2px", px: "2px", backgroundColor: bgcolor, borderRadius: "20px" }}>
       {matches.map((match, index) => {
-        const {teams, goals} = match 
-        const [teamA, teamB] = teams 
+        const { teams, goals } = match
+        const [teamA, teamB] = teams
         const [goalsA, goalsB] = goals
         const aGoals = typeof goalsA === "number" ? `${goalsA}` : "-"
         const bGoals = typeof goalsB === "number" ? `${goalsB}` : "-"
-          return (
-          <tr key={index} style={{ height: "14px" }}>
-            <td><Flag code={teamA} title={participants(teamA)} style={{ height: "14px", marginRight: "8px"}} fallback={<span>🏴‍☠️</span>} /></td>
-            <td><Flag code={teamB} title={participants(teamB)} style={{ height: "14px", marginRight: "16px"}} fallback={<span>🏴‍☠️</span>} /></td>
-            <td><span style={{ fontSize: "14px", lineHeight: "16px", color: "black" }}>{`${aGoals}:${bGoals}`}</span></td>
+        return (
+          <tr key={index} sx={{ height: "14px" }}>
+            <td><Flag code={teamA} title={participants(teamA)} sx={{ height: "14px", marginRight: "8px" }} fallback={<span>🏴‍☠️</span>} /></td>
+            <td><Flag code={teamB} title={participants(teamB)} sx={{ height: "14px", marginRight: "16px" }} fallback={<span>🏴‍☠️</span>} /></td>
+            <td><span sx={{ fontSize: 0, lineHeight: "16px", color: "ft" }}>{`${aGoals}:${bGoals}`}</span></td>
           </tr>
-          )
+        )
       })}
     </table>
   )
--- src/components/helpers.js
+++ src/components/helpers.js
@@ -1,14 +1,14 @@
 export function getAssociation(winningCombination) {
-  switch(winningCombination) {
+  switch (winningCombination) {
     case "ABCD":
     case "ABCE":
     case "ABCF":
-      return [3,7,5,2]
+      return [3, 7, 5, 2]
     case "ABDE":
     case "ABDF":
-      return [7,5,3,2]
+      return [7, 5, 3, 2]
     case "ABEF":
-      return [5,7,3,2]
+      return [5, 7, 3, 2]
     case "ACDE":
     case "ACDF":
     case "ADEF":
@@ -16,10 +16,22 @@ export function getAssociation(winningCombination) {
     case "BCEF":
     case "BDEF":
     case "CDEF":
-      return [5,7,2,3]
+      return [5, 7, 2, 3]
     case "BCDE":
-      return [7,5,2,3] 
+      return [7, 5, 2, 3]
     default:
       return []
+  }
+}
+
+export const getColor = (number) => {
+  switch (number) {
+    case 0: return "#7AACAC"
+    case 1: return "#CBD0CC"
+    case 2: return "greenyellow"
+    case 3: return "#E6D2B5"
+    case 4: return "#EAF044"
+    case 5: return "#D79FA6"
+    default: return "bg"
   }
 }
