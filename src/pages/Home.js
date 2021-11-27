a3daa8be187bf55d5dc60da6295b3ad904eb2f25
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-11-27 11:14:34 +0100

    add rudimentary main page

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+function Home() {
+  return (
+    <div>
+      <h1>Overview</h1>
+      <ul>
+        <Link to="/euro20">Euro 2020</Link>
+      </ul>
+    </div>
+  )
+}
+
+export default Home
