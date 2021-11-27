6d1bc5e0e84cf4d813c1d44cad27ec1e66069493
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-11-27 11:14:48 +0100

    change routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React, { Suspense, lazy } from "react"
 import { HashRouter as Router, Switch, Route } from "react-router-dom"
 const Euro20 = lazy(() => import("../pages/Euro20"))
+const Home = lazy(() => import("../pages/Home"))
 
 function App() {
   return (
     <Router basename={`/${process.env.PUBLIC_URL}`}>
       <Suspense fallback={<div>Loading...</div>}>
         <Switch>
-          <Route path="/">
+          <Route path="/euro20">
             <Euro20 />
+          </Route>
+          <Route path="/" >
+            <Home />
           </Route>
         </Switch>
       </Suspense>
