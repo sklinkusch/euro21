67d94c7609a31a7f94f987ed25b69911e15edec1
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-07-09 10:50:06 +0200

    add correct flag of Northern Ireland

--- src/components/FlagWrapper.js
+++ src/components/FlagWrapper.js
@@ -1,8 +1,12 @@
 /** @jsxImportSource theme-ui */
 import React from 'react'
 import Flag from "react-world-flags"
+import GB_NIR from "../images/GB_NIR.svg"
 
 function FlagWrapper({ team, participant }) {
+  if (team === "GB-NIR") {
+    return <img src={GB_NIR} alt={participant} title={participant} sx={{ height: "14px", maxWidth: "22px", marginRight: "8px" }} />
+  }
   return (
     <Flag
       code={team}
