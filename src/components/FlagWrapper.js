7226ed231fdb49cd6afb21a01958817596fac078
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-07-31 23:07:22 +0200

    add some ancient flags

--- src/components/FlagWrapper.js
+++ src/components/FlagWrapper.js
@@ -2,23 +2,34 @@
 import React from 'react'
 import Flag from "react-world-flags"
 import GB_NIR from "../images/GB_NIR.svg"
+import SU from "../images/SU.svg"
+import SU_CIS from "../images/SU_CIS.svg"
+import YU_SCG from "../images/YU_SCG.svg"
 
 function FlagWrapper({ team, participant }) {
-  if (team === "GB-NIR") {
-    return <img src={GB_NIR} alt={participant} title={participant} sx={{ height: "14px", maxWidth: "22px", marginRight: "8px" }} />
+  switch(team) {
+    case "GB-NIR":
+      return <img src={GB_NIR} alt={participant} title={participant} sx={{ height: "14px", maxWidth: "22px", marginRight: "8px" }} />
+    case "SU":
+      return <img src={SU} alt={participant} title={participant} sx={{ height: "14px", maxWidth: "22px", marginRight: "8px" }} />
+    case "SU-CIS":
+      return <img src={SU_CIS} alt={participant} title={participant} sx={{ height: "14px", maxWidth: "22px", marginRight: "8px" }} />
+    case "YU-SCG":
+      return <img src={YU_SCG} alt={participant} title={participant} sx={{ height: "14px", maxWidth: "22px", marginRight: "8px" }} />
+    default:
+      return (
+        <Flag
+          code={team}
+          title={participant}
+          sx={{
+            height: "14px",
+            maxWidth: "22px",
+            marginRight: "8px"
+          }}
+          fallback={<span>🏴‍☠️</span>}
+        />
+      )
   }
-  return (
-    <Flag
-      code={team}
-      title={participant}
-      sx={{
-        height: "14px",
-        maxWidth: "22px",
-        marginRight: "8px"
-      }}
-      fallback={<span>🏴‍☠️</span>}
-    />
-  )
 }
 
 export default FlagWrapper
