310ea0a6e16b3bf56c0892c1a17ef314d66ef2bb
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-08-02 21:34:16 +0200

    add routing for Men's World Cup 2006

--- src/components/App.js
+++ src/components/App.js
@@ -1,64 +1,106 @@
 import React, { Suspense, lazy } from "react"
 import { HashRouter as Router, Switch, Route } from "react-router-dom"
-const WEuro17 = lazy(() => import("../pages/WEuro17")) 
-const WEuro22 = lazy(() => import("../pages/WEuro22"))
-const WWC15 = lazy(() => import("../pages/WWorldCup2015"))
-const WWC19 = lazy(() => import("../pages/WWorldCup2019"))
-const WC22 = lazy(() => import("../pages/WorldCup2022"))
-const WC18 = lazy(() => import("../pages/WorldCup2018"))
-const WC14 = lazy(() => import("../pages/WorldCup2014"))
-const WC10 = lazy(() => import("../pages/WorldCup2010"))
-const Euro20 = lazy(() => import("../pages/Euro20"))
-const Euro16 = lazy(() => import("../pages/Euro16"))
-const Euro12 = lazy(() => import("../pages/Euro12"))
-const Euro08 = lazy(() => import("../pages/Euro08"))
-const Euro04 = lazy(() => import("../pages/Euro04"))
+const WEuro2017 = lazy(() => import("../pages/WEuro17")) 
+const WEuro2022 = lazy(() => import("../pages/WEuro22"))
+const WWC2015 = lazy(() => import("../pages/WWorldCup2015"))
+const WWC2019 = lazy(() => import("../pages/WWorldCup2019"))
+const WC2022 = lazy(() => import("../pages/WorldCup2022"))
+const WC2018 = lazy(() => import("../pages/WorldCup2018"))
+const WC2014 = lazy(() => import("../pages/WorldCup2014"))
+const WC2010 = lazy(() => import("../pages/WorldCup2010"))
+const WC2006 = lazy(() => import("../pages/WorldCup2006"))
+const Euro2020 = lazy(() => import("../pages/Euro20"))
+const Euro2016 = lazy(() => import("../pages/Euro16"))
+const Euro2012 = lazy(() => import("../pages/Euro12"))
+const Euro2008 = lazy(() => import("../pages/Euro08"))
+const Euro2004 = lazy(() => import("../pages/Euro04"))
 const Home = lazy(() => import("../pages/Home"))
+
+const MensWorldCups = [
+  {
+    path: "/wc2022",
+    Component: WC2022
+  },
+  {
+    path: "/wc2018",
+    Component: WC2018
+  },
+  {
+    path: "/wc2014",
+    Component: WC2014
+  },
+  {
+    path: "/wc2010",
+    Component: WC2010
+  },
+  {
+    path: "/wc2006",
+    Component: WC2006
+  }
+]
+
+const WomensWorldCups = [
+  {
+    path: "/wwc2019",
+    Component: WWC2019
+  },
+  {
+    path: "/wwc2015",
+    Component: WWC2015
+  }
+]
+
+const MensEuros = [
+  {
+    path: "/euro2020",
+    Component: Euro2020
+  },
+  {
+    path: "/euro2016",
+    Component: Euro2016
+  },
+  {
+    path: "/euro2012",
+    Component: Euro2012
+  },
+  {
+    path: "/euro2008",
+    Component: Euro2008
+  },
+  {
+    path: "/euro2004",
+    Component: Euro2004
+  }
+]
+
+const WomensEuros = [
+  {
+    path: "/weuro2022",
+    Component: WEuro2022
+  },
+  {
+    path: "/weuro2017",
+    Component: WEuro2017
+  }
+]
+
+const tournaments = [
+  ...MensWorldCups,
+  ...WomensWorldCups,
+  ...MensEuros,
+  ...WomensEuros
+]
 
 function App() {
   return (
     <Router basename={`/${process.env.PUBLIC_URL}`}>
       <Suspense fallback={<div>Loading...</div>}>
         <Switch>
-          <Route path="/wc22">
-            <WC22 />
-          </Route>
-          <Route path="/wc18">
-            <WC18 />
-          </Route>
-          <Route path="/wc14">
-            <WC14 />
-          </Route>
-          <Route path="/wc10">
-            <WC10 />
-          </Route>
-          <Route path="/wwc19">
-            <WWC19 />
-          </Route>
-          <Route path="/wwc15">
-            <WWC15 />
-          </Route>
-          <Route path="/euro20">
-            <Euro20 />
-          </Route>
-          <Route path="/euro16">
-            <Euro16 />
-          </Route>
-          <Route path="/euro12">
-            <Euro12 />
-          </Route>
-          <Route path="/euro08">
-            <Euro08 />
-          </Route>
-          <Route path="/euro04">
-            <Euro04 />
-          </Route>
-          <Route path="/weuro22">
-            <WEuro22 />
-          </Route>
-          <Route path="/weuro17">
-            <WEuro17 />
-          </Route>
+          {tournaments.map((tournament, index) => (
+            <Route path={tournament.path} key={index}>
+              <tournament.Component />
+            </Route>
+          ))}
           <Route path="/" >
             <Home />
           </Route>
