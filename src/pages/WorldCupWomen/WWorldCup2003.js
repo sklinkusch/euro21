14216d1ef10f34f69608e4e40583887a91b3350a
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-12-09 19:19:57 +0100

    fix styling of World Cup 2003

--- src/pages/WorldCupWomen/WWorldCup2003.js
+++ src/pages/WorldCupWomen/WWorldCup2003.js
@@ -103,8 +103,8 @@ function WWC03() {
             modus="wc"
           />
            <div sx={{ display: "grid", gridTemplateColumns: ["repeat(2,1fr)", "repeat(2,1fr)", "repeat(6,1fr)"] }}>
-            <Viertelfinale teams={vfTeams} matchvf={matchvf} />
-            <Halbfinale teams={hfTeams} matchhf={matchhf} />
+            <Viertelfinale teams={vfTeams} matchvf={matchvf} maxColumns={6} />
+            <Halbfinale teams={hfTeams} matchhf={matchhf} full={true} />
             <Platz3 teams={teams3P} match3p={match3p} />
            <Finale teams={fTeams} matchf={matchf} />
           </div> 
