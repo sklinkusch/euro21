27bcc21aeaa98d569060c2eb95ba2018342e1b5d
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-12-10 17:09:37 +0100

    add dates for World Cup 2023

--- src/data/WorldCupWomen/matches_wwc2023.js
+++ src/data/WorldCupWomen/matches_wwc2023.js
@@ -5,256 +5,304 @@ const matches = {
     {
       teams: ["NZ", "NO"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-20T07:00:00Z"
     },
     {
       teams: ["PH", "CH"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-21T05:00:00Z"
     },
     {
       teams: ["NZ", "PH"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-25T05:30:00Z"
     },
     {
       teams: ["CH", "NO"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-25T08:00:00Z"
     },
     {
       teams: ["NO", "PH"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-30T07:00:00Z"
     },
     {
       teams: ["CH", "NZ"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-30T07:00:00Z"
     },
   ],
   B: [
     {
       teams: ["AU", "IE"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-20T10:00:00Z"
     },
     {
       teams: ["NG", "CA"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-21T02:30:00Z"
     },
     {
       teams: ["CA", "IE"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-26T12:00:00Z"
     },
     {
       teams: ["AU", "NG"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-27T10:00:00Z"
     },
     {
       teams: ["IE", "NG"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-31T10:00:00Z"
     },
     {
       teams: ["CA", "AU"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-31T10:00:00Z"
     },
   ],
   C: [
     {
       teams: ["ES", "CR"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-21T07:30:00Z"
     },
     {
       teams: ["ZM", "JP"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-22T07:00:00Z"
     },
     {
       teams: ["JP", "CR"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-26T05:00:00Z"
     },
     {
       teams: ["ES", "ZM"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-26T07:30:00Z"
     },
     {
       teams: ["CR", "ZM"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-31T07:00:00Z"
     },
     {
       teams: ["JP", "ES"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-31T07:00:00Z"
     },
   ],
   D: [
     {
       teams: ["GB-ENG", "Q23B"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-22T09:30:00Z"
     },
     {
       teams: ["DK", "CN"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-22T12:00:00Z"
     },
     {
       teams: ["CN", "Q23B"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-28T08:30:00Z"
     },
     {
       teams: ["GB-ENG", "DK"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-28T11:00:00Z"
     },
     {
       teams: ["Q23B", "DK"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-08-01T11:00:00Z"
     },
     {
       teams: ["CN", "GB-ENG"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-08-01T11:00:00Z"
     },
   ],
   E: [
     {
       teams: ["US", "VN"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-22T01:00:00Z"
     },
     {
       teams: ["NL", "Q23A"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-23T07:30:00Z"
     },
     {
       teams: ["Q23A", "VN"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-27T01:00:00Z"
     },
     {
       teams: ["US", "NL"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-27T07:30:00Z"
     },
     {
       teams: ["VN", "NL"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-08-01T07:00:00Z"
     },
     {
       teams: ["Q23A", "US"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-08-01T07:00:00Z"
     },
   ],
   F: [
     {
       teams: ["FR", "JM"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-23T10:00:00Z"
     },
     {
       teams: ["BR", "Q23C"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-24T11:00:00Z"
     },
     {
       teams: ["Q23C", "JM"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-29T10:00:00Z"
     },
     {
       teams: ["FR", "BR"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-29T12:30:00Z"
     },
     {
       teams: ["JM", "BR"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-08-02T10:00:00Z"
     },
     {
       teams: ["Q23C", "FR"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-08-02T10:00:00Z"
     },
   ],
   G: [
     {
       teams: ["SE", "ZA"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-23T05:00:00Z"
     },
     {
       teams: ["IT", "AR"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-24T06:00:00Z"
     },
     {
       teams: ["AR", "ZA"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-28T00:00:00Z"
     },
     {
       teams: ["SE", "IT"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-29T07:30:00Z"
     },
     {
       teams: ["ZA", "IT"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-08-02T07:00:00Z"
     },
     {
       teams: ["AR", "SE"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-08-02T07:00:00Z"
     },
   ],
   H: [
     {
       teams: ["DE", "MA"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-24T08:30:00Z"
     },
     {
       teams: ["CO", "KR"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-25T02:00:00Z"
     },
     {
       teams: ["KR", "MA"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-30T04:30:00Z"
     },
     {
       teams: ["DE", "CO"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-07-30T09:30:00Z"
     },
     {
       teams: ["MA", "CO"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-08-03T10:00:00Z"
     },
     {
       teams: ["KR", "DE"],
       goals: [null, null],
-      fairPlay: [null, null]
+      fairPlay: [null, null],
+      date: "2023-08-03T10:00:00Z"
     },
   ]
 }
@@ -262,77 +310,93 @@ const matches = {
 export const matchaf = [
   {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-05T05:00:00Z"
   },
   {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-05T08:00:00Z"
   },
   {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-06T02:00:00Z"
   },
   {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-06T09:00:00Z"
   },
   {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-07T07:30:00Z"
   },
   {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-07T10:30:00Z"
   },
   {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-08T08:00:00Z"
   },
   {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-08T11:00:00Z"
   },
 ]
 
 export const matchvf = [
   {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-11T01:00:00Z"
   },
   {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-11T07:30:00Z"
   },
   {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-12T07:00:00Z"
   },
   {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-12T10:30:00Z"
   },
 ]
 
 export const matchhf = [
   {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-15T08:00:00Z"
   },
   {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-16T10:00:00Z"
   },
 ]
 
 export const match3p = {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-19T08:00:00Z"
 }
 
 
 export const matchf = {
     goals: [null, null],
-    add: null
+    add: null,
+    date: "2023-08-20T10:00:00Z"
 }
 
 export default matches
