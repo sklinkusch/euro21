de632d5dd138ca929478c35b8d64e24fc58f5bfc
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-12-16 22:24:54 +0100

    link all schemas

--- cms/schemas/index.ts
+++ cms/schemas/index.ts
@@ -1,1 +1,8 @@
-export const schemaTypes = []
+import TournamentSeries from './TournamentSeries'
+import Tournament from './Tournament'
+import MatchResult from './MatchResult'
+import Group from './Group'
+import Match from './Match'
+import Team from './Team'
+
+export const schemaTypes = [Team, Group, Match, MatchResult, Tournament, TournamentSeries]
