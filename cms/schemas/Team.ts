96cc5343998399ac755431b5a0e626f85e208f5b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2022-12-16 22:24:42 +0100

    add schema for Team

---
+++ cms/schemas/Team.ts
@@ -0,0 +1,25 @@
+const Team = {
+  name: 'Team',
+  title: 'Mannschaft',
+  type: 'string',
+  list: [
+    {title: 'Afghanistan', value: 'AF'},
+    {title: 'Ägypten', value: 'EG'},
+    {title: 'Albanien', value: 'AL'},
+    {title: 'Algerien', value: 'DZ'},
+    {title: 'Amerikanisch-Samoa', value: 'AS'},
+    {title: 'Andorra', value: 'AD'},
+    {title: 'Angola', value: 'AO'},
+    {title: 'Anguilla', value: 'AI'},
+    {title: 'Antigua und Barbuda', value: 'AG'},
+    {title: 'Äquatorialguinea', value: 'GQ'},
+    {title: 'Argentinien', value: 'AR'},
+    {title: 'Armenien', value: 'AM'},
+    {title: 'Aruba', value: 'AW'},
+    {title: 'Aserbaidschan', value: 'AZ'},
+    {title: 'Äthiopien', value: 'ET'},
+    {title: 'Australien', value: 'AU'},
+  ],
+}
+
+export default Team
