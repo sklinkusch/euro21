c4cd707949644d4c5ab999b067cdebae4ba556b4
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-02-24 20:26:44 +0100

    change url for world cup 2018

--- src/pages/WorldCupMen/WorldCup2018.js
+++ src/pages/WorldCupMen/WorldCup2018.js
@@ -21,7 +21,7 @@ function WorldCup2018() {
     async function fetchData () {
       const longLang = navigator && "language" in navigator ? navigator.language : ""
       const lang = longLang.length > 0 ? longLang.substring(0,2) : "en"
-      const response = await fetch(`https://euro21-api.vercel.app/worldcup?year=2018&lang=${lang}`)
+      const response = await fetch(`https://euro21-api.vercel.app/worldcupmen?year=2018&lang=${lang}`)
       const data = await response.json()
       setMatches(await data)
     }
