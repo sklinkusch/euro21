a59508ab5617c0b79ba1faf73eb3287b0852278b
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-03-08 15:52:44 +0100

    add styles for large

--- src/components/FlagWrapper.js
+++ src/components/FlagWrapper.js
@@ -7,28 +7,27 @@ import SU_CIS from "../images/SU_CIS.svg"
 import TW_OLY from "../images/TW_OLY.svg"
 import YU_SCG from "../images/YU_SCG.svg"
 
-function FlagWrapper({ team, participant }) {
+function FlagWrapper({ team, participant, large = false }) {
+  const styles = large ? { height: "21px", maxWidth: "30px", mr: "8px" } : { height: "14px", maxWidth: "22px", mr: "8px" }
   switch(team) {
     case "GB-NIR":
-      return <img src={GB_NIR} alt={participant} title={participant} sx={{ height: "14px", maxWidth: "22px", marginRight: "8px" }} />
+      return <img src={GB_NIR} alt={participant} title={participant} sx={{ ...styles }} />
     case "SU":
-      return <img src={SU} alt={participant} title={participant} sx={{ height: "14px", maxWidth: "22px", marginRight: "8px" }} />
+      return <img src={SU} alt={participant} title={participant} sx={{ ...styles }} />
     case "SU-CIS":
-      return <img src={SU_CIS} alt={participant} title={participant} sx={{ height: "14px", maxWidth: "22px", marginRight: "8px" }} />
+      return <img src={SU_CIS} alt={participant} title={participant} sx={{ ...styles }} />
     case "TW-OLY":
-      return <img src={TW_OLY} alt={participant} title={participant} sx={{ height: "14px", maxWidth: "22px", marginRight: "8px" }} />
+      return <img src={TW_OLY} alt={participant} title={participant} sx={{ ...styles }} />
     case "YU-BR":
     case "YU-SCG":
-      return <img src={YU_SCG} alt={participant} title={participant} sx={{ height: "14px", maxWidth: "22px", marginRight: "8px" }} />
+      return <img src={YU_SCG} alt={participant} title={participant} sx={{ ...styles }} />
     default:
       return (
         <Flag
           code={team}
           title={participant}
           sx={{
-            height: "14px",
-            maxWidth: "22px",
-            marginRight: "8px"
+            ...styles
           }}
           fallback={<span>🏴‍☠️</span>}
         />
