c1a87860af8bb8b662b52b7a4862bb6a1ab59a5d
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2023-03-11 17:52:32 +0100

    embed flag and country sources

--- src/components/FlagWrapper.js
+++ src/components/FlagWrapper.js
@@ -6,12 +6,17 @@ import SU from "../images/SU.svg"
 import SU_CIS from "../images/SU_CIS.svg"
 import TW_OLY from "../images/TW_OLY.svg"
 import YU_SCG from "../images/YU_SCG.svg"
+import YU_SFR from "../images/YU_SFR.svg"
 
 function FlagWrapper({ team, participant, large = false }) {
   const styles = large ? { height: "21px", maxWidth: "30px", minWidth: "21px", mr: "8px" } : { height: "14px", maxWidth: "22px", minWidth: "14px", mr: "8px" }
   switch(team) {
     case "GB-NIR":
       return <img src={GB_NIR} alt={participant} title={participant} sx={{ ...styles }} />
+    case "CS":
+      return <Flag code='CS' alt={participant} title={participant} sx={{ ...styles }} />
+    case "DE-BRD":
+      return <Flag code='DE' alt={participant} title={participant} sx={{ ...styles }} />
     case "SU":
       return <img src={SU} alt={participant} title={participant} sx={{ ...styles }} />
     case "SU-CIS":
@@ -21,6 +26,8 @@ function FlagWrapper({ team, participant, large = false }) {
     case "YU-BR":
     case "YU-SCG":
       return <img src={YU_SCG} alt={participant} title={participant} sx={{ ...styles }} />
+    case "YU-SFR":
+      return <img src={YU_SFR} alt={participant} title={participant} sx={{ ...styles }} />
     default:
       return (
         <Flag
